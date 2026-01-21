cc4c251bafc3b8cc20902cebb3362fdbef6e9abc
MrDicktor <bosstas220@gmail.com>
2026-01-21 16:31:40 +0200

    bot remembers user group

--- bot.py
+++ bot.py
@@ -8,23 +8,58 @@ import pars
 load_dotenv()
 TOKEN = os.getenv("TOKEN")
 
+IDS = {}
+
+def load_ids():
+    IDS.clear()
+    with open("IDS", "r", encoding="utf-8") as f:
+        ids = f.read()
+    ids = ids.split("\n")
+    for user_id in ids:
+        if not user_id:
+            continue
+        user_id = user_id.split(" ")
+        IDS[user_id[0]] = user_id[1]
+    print(IDS)
 
 async def start(update, context):
-    await update.message.reply_text("Введіть назву групи")
-    return 2
+    load_ids()
+    user_id = str(update.effective_user.id)
+    if user_id not in IDS:
+        await update.message.reply_text("Введіть назву групи")
+        return 2
+    else:
+        context.user_data["group"] = IDS[user_id]
+        pars.parsing(IDS[user_id])
+        await show_saved(update, context)
+        return ConversationHandler.END
+
+
+async def show_saved(update, context):
+    file_name = context.user_data["group"] + ".txt"
+    with open(file_name, "r", encoding="utf-8") as f:
+        file_content = f.read()
+    await update.message.reply_text(file_content[:4000])
+
+    return ConversationHandler.END
+
+
+
 
 
 
 async def show_data(update, context):
     result = await pars.save_data(update, context)
-
-    if result == 1:
+    if result == "not found":
         await update.message.reply_text(
             "Групу не знайдено або розкладу немає 😕\n\n"
             "Спробуйте ще раз. Введіть назву групи:"
         )
         return 2
-
+    user_id = update.effective_user.id
+    with open("IDS", "a", encoding="utf-8") as f:
+        f.write(str(user_id) +  " " + context.user_data["group"] + "\n")
+    IDS[user_id] = context.user_data["group"]
     file_name = context.user_data["group"] + ".txt"
     with open(file_name, "r", encoding="utf-8") as f:
         file_content = f.read()
--- pars.py
+++ pars.py
@@ -4,12 +4,17 @@ import requests
 from urllib.parse import unquote, quote
 #"cp1251"
 
+url = "https://ps.vnu.edu.ua/cgi-bin/timetable.cgi?n=700"
+markers1 = ['зав', 'ст.', 'проф.', 'доц.', 'асист']
+aud = 'ауд'
+groups = ['Збірна група', 'Потік']
+spec = "Ліквідація"
 
 
 async def save_data(update, context):
-    url = "https://ps.vnu.edu.ua/cgi-bin/timetable.cgi?n=700"
     writing = ""
     group = update.message.text
+    print(update.effective_user.id)
     context.user_data["group"] = group
     encoded_group = quote(group, encoding='cp1251')
     data = "faculty=0&teacher=&course=0&group=" + encoded_group + "&sdate=&edate=&n=700"
@@ -21,7 +26,7 @@ async def save_data(update, context):
 
     week_days = week.find_all("div", class_="col-md-6 col-sm-6 col-xs-12 col-print-6")
     if not week_days:
-        return 1
+        return "not found"
 
     for weekday in week_days:
         date = weekday.find("h4")
@@ -34,10 +39,6 @@ async def save_data(update, context):
             num = f"{row[0]}\ufe0f\u20e3"
             row = num + " " + row[1:6] + " — " + row[6:11] + " \n" + "📚" + row[11:] + " \n"
             row = row.replace('\xa0', ' ')
-            markers1 = ['зав', 'ст.', 'проф.', 'доц.', 'асист']
-            aud = 'ауд'
-            groups = ['Збірна група', 'Потік']
-            spec = "Ліквідація"
             for marker in markers1:
                 if marker in row:
                     row = row.replace(marker, "\n" + "👨‍🏫 " + marker, 1)
@@ -48,7 +49,45 @@ async def save_data(update, context):
                 row = row.replace(gr, "\n" + "🥷 " + gr, 1)
             writing += row + "\n"
         writing += "➖" * 14 + "\n" + "\n"
-        print(writing)
+    with open(group + ".txt", "w", encoding="utf-8") as f:
+        f.write(writing)
+    return 3
+
+def parsing(group):
+    writing = ""
+    encoded_group = quote(group, encoding='cp1251')
+    data = "faculty=0&teacher=&course=0&group=" + encoded_group + "&sdate=&edate=&n=700"
+    response = requests.post(url, data=data)
+    response.encoding = 'cp1251'
+    soup = BeautifulSoup(response.text, "lxml")
+    tables = soup.find_all("div", class_="container")
+    week = tables[1]
+
+    week_days = week.find_all("div", class_="col-md-6 col-sm-6 col-xs-12 col-print-6")
+    if not week_days:
+        return "not found"
+
+    for weekday in week_days:
+        date = weekday.find("h4")
+        writing += date.text + "\n" + "\n"
+        schedule = weekday.find_all("tr")
+        for tr in schedule:
+            row = tr.text
+            if len(row) < 13:
+                continue
+            num = f"{row[0]}\ufe0f\u20e3"
+            row = num + " " + row[1:6] + " — " + row[6:11] + " \n" + "📚" + row[11:] + " \n"
+            row = row.replace('\xa0', ' ')
+            for marker in markers1:
+                if marker in row:
+                    row = row.replace(marker, "\n" + "👨‍🏫 " + marker, 1)
+                    break
+            row = row.replace(aud, "\n" + "🏫 " + aud, 1)
+            row = row.replace(spec, "\n" + spec, 1)
+            for gr in groups:
+                row = row.replace(gr, "\n" + "🥷 " + gr, 1)
+            writing += row + "\n"
+        writing += "➖" * 14 + "\n" + "\n"
     with open(group + ".txt", "w", encoding="utf-8") as f:
         f.write(writing)
     return 3
